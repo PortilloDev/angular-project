a40408bc447e255ad611b75a9bbcbd84f17ccdf7
Iván Portillo <ivan.portillo@unir.net>
2022-12-21 07:02:23 +0100

    creación de routes en modulos

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,31 @@
+import { Routes, RouterModule } from "@angular/router"
+import { NgModule } from "@angular/core"
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { GraficalComponent } from "./grafical/grafical.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { PagesComponent } from "./pages.component";
+
+const routes: Routes = [
+ {
+    path: 'dashboard',
+    component: PagesComponent,
+    children: [
+      { path: '', component : DashboardComponent},
+      { path: 'progress', component : ProgressComponent},
+      { path: 'grafical', component : GraficalComponent},
+    ]
+},
+];
+
+
+@NgModule({
+  imports:[
+    RouterModule.forChild(routes),
+
+  ],
+
+  exports:[RouterModule]
+})
+
+export class PagesRoutingModule{}
+
